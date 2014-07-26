28331ce8961a1f9b75bc0e9330f87d8c40e84f0f
BenchR267 <herzog.benjamin@me.com>
2014-07-26 11:20:58 +0200

    inital commit

---
+++ AutoLayout-Tutorial/AutoLayout-Tutorial/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  AutoLayout-Tutorial
+//
+//  Created by Benjamin Herzog on 25.07.14.
+//  Copyright (c) 2014 Benjamin Herzog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell
+        
+        return cell
+    }
+
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(tableView: UITableView!, heightForRowAtIndexPath indexPath: NSIndexPath!) -> CGFloat {
+        return 80
+    }
+
+}
+
